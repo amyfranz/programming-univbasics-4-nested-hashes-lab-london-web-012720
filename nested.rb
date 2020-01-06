9a243366241e3fffd2b57e2f7574e2cca6adc609
Amy Franz <amy.franz@gmail.com>
2020-01-06 14:37:56 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -57,7 +57,6 @@ def dennis_ritchies_language
 end
 
 def adding_matz
-#add the following information to the top level of programmer_hash
 	programmer_hash = 
  		{
         :grace_hopper => {
@@ -73,12 +72,12 @@ def adding_matz
           :languages => ["C"]
         }
      }
-
-programmer_hash[:yukihiro_matsumoto] = {
-:known_for => "Ruby",
-:languages => ["LISP", "C"]
-}
-return programmer_hash
+     
+  programmer_hash[:yukihiro_matsumoto] = {
+    :known_for => "Ruby",
+    :languages => ["LISP", "C"]
+  }
+  return programmer_hash
 end
 
 def changing_alan
@@ -100,6 +99,7 @@ def changing_alan
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
      programmer_hash[:alan_kay][:known_for] = alans_new_info
+     return programmer_hash
 end
 
 def adding_to_dennis
